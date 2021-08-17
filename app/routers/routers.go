5f5719d1159bc9de8ec61516a7f97aea0eb544b0
Ahmad Shobirin <ahmadshobirin.dev@gmail.com>
2021-08-17 16:48:29 +0700

    Features/auth (#15)
    * user
    
    * auth login
    
    * register
    
    * fix response
    
    * fix response
    
    * add middleware

--- app/routers/routers.go
+++ app/routers/routers.go
@@ -21,6 +21,12 @@ type ControllerList struct {
 func (cl *ControllerList) RouteRegister(e *echo.Echo) {
 	r := e.Group("/api/v1")
 
+	authRouter := r.Group("/auth")
+	authRouter.POST("/login", cl.AuthController.Login)
+	authRouter.POST("/register", cl.AuthController.Register)
+
+	r.Use(middleware.JWTWithConfig(cl.JWTMiddleware))
+
 	cityRouter := r.Group("/city")
 	cityRouter.GET("", cl.CityController.Find)
 	cityRouter.GET("/:id", cl.CityController.FindByID)
@@ -31,8 +37,4 @@ func (cl *ControllerList) RouteRegister(e *echo.Echo) {
 
 	userRouter := r.Group("/user")
 	userRouter.POST("", cl.UserController.Store)
-
-	authRouter := r.Group("/auth")
-	authRouter.POST("/login", cl.AuthController.Login)
-	authRouter.POST("/register", cl.AuthController.Register)
 }
